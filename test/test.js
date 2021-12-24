e9401df4175519c79457513190f69b3a14356465
Victoria Prater <developerm216@gmail.com>
2021-12-24 08:02:30 -0500

    Completed Version
    Completed Version

---
+++ test/test.js
@@ -0,0 +1,34 @@
+import test from 'ava';
+import isPlainObj from 'is-plain-obj';
+import eslint from 'eslint';
+import tempWrite from 'temp-write';
+
+const hasRule = (errors, ruleId) => errors.some(x => x.ruleId === ruleId);
+
+function runEslint(str, conf) {
+	const linter = new eslint.CLIEngine({
+		useEslintrc: false,
+		configFile: tempWrite.sync(JSON.stringify(conf))
+	});
+
+	return linter.executeOnText(str).results[0].messages;
+}
+
+test('main', t => {
+	const conf = require('../');
+
+	t.true(isPlainObj(conf));
+	t.true(isPlainObj(conf.rules));
+
+	const errors = runEslint('\'use strict\';\nconsole.log("unicorn")\n', conf);
+	t.true(hasRule(errors, 'quotes'));
+});
+
+test('browser', t => {
+	const conf = require('../browser');
+
+	t.true(isPlainObj(conf));
+
+	const errors = runEslint('\'use strict\';\nprocess.exit();\n', conf);
+	t.true(hasRule(errors, 'no-undef'));
+});
